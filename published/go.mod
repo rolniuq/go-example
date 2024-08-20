5d85343c07e3822b202115910c67a18c3bac2f85
rolnuiqdev <truongquynh2525@gmail.com>
2024-08-20 16:50:55 +0700

    chore: use my go published package

---
+++ published/go.mod
@@ -0,0 +1,5 @@
+module published
+
+go 1.21.0
+
+require github.com/rolniuq/mypackage v0.0.0-20240820094901-1650913009fd // indirect
---
+++ published/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rolniuq/mypackage"
+)
+
+func main() {
+	a := mypackage.SayHello()
+	fmt.Println(a)
+}
