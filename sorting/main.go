30d273bc23833cea2c9f2f46e7efecb77f25e886
rolnuiqdev <truongquynh2525@gmail.com>
2024-08-25 10:25:58 +0700

    chore: sort string

--- sorting/main.go
+++ sorting/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"slices"
+	"sort"
 )
 
 func main() {
 	strs := []string{"c", "a", "b"}
 	slices.Sort(strs)
 	log.Println(strs)
+
+	str := "acb"
+	strRunes := []rune(str)
+	sort.Slice(strRunes, func(i, j int) bool {
+		return strRunes[i] < strRunes[j]
+	})
+	log.Println(str)
 
 	ints := []int{7, 2, 4}
 	slices.Sort(ints)
