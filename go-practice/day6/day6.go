a6fcec154fb82bad21ac2fb5495141095d24b576
rolnuiqdev <truongquynh2525@gmail.com>
2025-05-12 08:10:15 +0700

    chore: 12 May

--- go-practice/day6/day6.go
+++ go-practice/day6/day6.go
@@ -53,16 +53,55 @@ func (d *Day6) Process(ctx context.Context) {
 	fmt.Printf("Hello, %s!\n", user.(string))
 }
 
-func (d *Day6) Exec() {
-	ctx := context.Background()
-	d.ContextTree(ctx)
-
-	ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+func (d *Day6) ContextTreeV2() {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(time.Second))
 	defer cancel()
-	if err := d.Fetch(ctx, "http://example.com"); err != nil {
-		fmt.Println(err)
-	}
 
-	ctx = context.WithValue(ctx, UserKey, "Quynh")
-	d.Process(ctx)
+	ctx1, cancel1 := context.WithTimeout(ctx, 800*time.Millisecond)
+	defer cancel1()
+
+	ctx2, cancel2 := context.WithTimeout(ctx, 500*time.Millisecond)
+	defer cancel2()
+
+	ctx3, cancel3 := context.WithTimeout(ctx, 300*time.Millisecond)
+	defer cancel3()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx3.Done():
+				fmt.Printf("ctx3 cancelled: %v\n", ctx3.Err())
+				return
+			case <-ctx2.Done():
+				fmt.Printf("ctx2 cancelled: %v\n", ctx2.Err())
+				return
+			case <-ctx1.Done():
+				fmt.Printf("ctx1 cancelled: %v\n", ctx1.Err())
+				return
+			case <-time.After(900 * time.Millisecond):
+				fmt.Println("timeout waiting")
+				return
+			}
+		}
+	}()
+	wg.Wait()
+}
+
+func (d *Day6) Exec() {
+	// ctx := context.Background()
+	// d.ContextTree(ctx)
+
+	// ctx, cancel := context.WithTimeout(ctx, 200*time.Millisecond)
+	// defer cancel()
+	// if err := d.Fetch(ctx, "http://example.com"); err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// ctx = context.WithValue(ctx, UserKey, "Quynh")
+	// d.Process(ctx)
+
+	d.ContextTreeV2()
 }
